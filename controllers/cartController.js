5a0a61b747e12667edc2533ea6d13595f26da263
Ar273404 <arun273404@gmail.com>
2024-07-08 17:14:18 +0530

    main

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,26 +4,7 @@ import Cart from "../models/addtocartModel.js";
 export const incrementCartItem = async (req, res) => {
      console.log("hello world");
  try {
-//    
-//     const { productId } = req.params; // Read productId from route parameters
-//     const userId = req.user;
 
-//     let cartItem = await Cart.findOne({ user: userId, product: productId });
-
-//     if (cartItem) {
-//       // Increment quantity if item already exists
-//       cartItem.quantity += 1;
-//     } else {
-//       // Create new cart item if it doesn't exist
-//       cartItem = new Cart({
-//         user: userId,
-//         product: productId,
-//         quantity: 1,
-//       });
-//     }
-
-//     await cartItem.save();
-//  
     res.status(200).json({ message: "Product added to cart", cartItem });
   } catch (error) {
     console.error(error);
@@ -33,31 +14,7 @@ export const incrementCartItem = async (req, res) => {
 
 // Decrement quantity of a product in the cart
 export const decrementCartItem = async (req, res) => {
-//   try {
-//     const { productId } = req.params; // Read productId from route parameters
-//     const userId = req.user;
 
-//     const cartItem = await Cart.findOne({
-//       user: userId,
-//       product: productId,
-//     });
-
-//     if (!cartItem) {
-//       return res.status(404).json({ message: "Product not found in cart" });
-//     }
-
-//     if (cartItem.quantity > 1) {
-//       cartItem.quantity -= 1;
-//       await cartItem.save();
-//     } else {
-//       await cartItem.remove();
-//     }
-
-//     res.status(200).json({ message: "Product quantity updated", cartItem });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Error updating cart", error });
-//   }
 };
 
 
--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -6,7 +6,9 @@ import multer from "multer"
 //Image Storage 
 
 const storage = multer.diskStorage({
-    destination:"uploads",
+    destination:(req,file,cb)=>{
+        cb(null,"uploads")
+    },
     filename:(req,file,cb)=>{
         return cb(null,`${Date.now()}${file.originalname}`)
     }
